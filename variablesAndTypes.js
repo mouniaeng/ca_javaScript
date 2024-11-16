28ebee384d13d61c0e665c6417e5a68f50caaa14
mouniaeng <132370600+mouniaeng@users.noreply.github.com>
2024-11-16 11:52:19 +0100

    Add files via upload

---
+++ variablesAndTypes.js
@@ -0,0 +1,6 @@
+const myNumber=4;
+const myString="Variables are great.";
+const myBoolean=false;
+console.log("myNumber is equal to " + myNumber);
+console.log("myString is equal to " + myString);
+console.log("myBoolean is equal to " + myBoolean);
